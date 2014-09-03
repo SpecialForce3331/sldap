b767621c5ad0b124fdfd67f9607a85bf1588ca97
Charlie Root <root@PIrouter.akvnzm.ru>
2014-09-04 00:31:22 +0600

    Исправил парсер

--- parser.py
+++ parser.py
@@ -6,13 +6,25 @@ server = "localhost"
 username = "ldap_squid"
 password = "qwerty"
 database = "ldap_squid"
-pathLog = "/var/log/squid/access.log";
+pathLog = "/var/log/squid/access.log"
 
-try:
-    conn = mysql.connector.connect(user=username, password=password, host=server, database=database)
-    cursor = conn.cursor()
-except:
-    exit("Can't connect to Mysql Server")
+conn = mysql.connector.connect(user=username, password=password, host=server, database=database)
+cursor = conn.cursor()
+
+def get_connection():
+    global conn
+    global cursor
+
+    try:
+        if not conn.is_connected():
+            conn.reconnect()
+            cursor = conn.cursor()
+            return conn, cursor
+        else:
+            return conn, cursor
+    except Exception as e:
+        print(e)
+        exit
 
 query = "SELECT login FROM users WHERE 1"
 cursor.execute(query)
@@ -31,7 +43,10 @@ else:
 
 def add_to_db( trafficArray ):
         query = "INSERT INTO usersTraffic ( login, site, bytes, dateTime ) VALUES "
-        print(len(trafficArray))
+
+        if len(trafficArray) < 1:
+            return
+
         for traffic in trafficArray:
             user_login, user_url, user_bytes, user_dateTime = traffic
 
@@ -39,6 +54,7 @@ def add_to_db( trafficArray ):
                 query = query + "(\"" + user_login + "\",\"" + user_url + "\",\"" + user_bytes + "\",\"" + user_dateTime + "\")"
             else:
                 query = query + "(\"" + user_login + "\",\"" + user_url + "\",\"" + user_bytes + "\",\"" + user_dateTime + "\"),"
+        conn, cursor = get_connection()
         cursor.execute(query)
         conn.commit()
 
@@ -73,32 +89,41 @@ for login in login_array:
     else:
         query = query + "\"" + login[0] + "\","
 
+conn, cursor = get_connection()
 cursor.execute(query)
 
 result = cursor.fetchall()
-
-query = "SELECT dateTime FROM usersTraffic ORDER BY id DESC LIMIT 1"
-cursor.execute(query)
-last_update = cursor.fetchone()[0]
 
 for row in result:
     last_update = row[0]
     current_traffic = row[1]
     login = row[2]
     query = "SELECT SUM(bytes) FROM usersTraffic WHERE dateTime > %s AND login = %s"
+    conn, cursor = get_connection()
     cursor.execute(query, (last_update, login))
     row = cursor.fetchone()
 
     if row[0] is not None:
         traffic = float(round(row[0]/1048576, 2)) + current_traffic
 
+        query = "SELECT dateTime FROM usersTraffic ORDER BY id DESC LIMIT 1"
+        conn, cursor = get_connection()
+        cursor.execute(query)
+        last_update = cursor.fetchone()[0]
+
         if traffic > 0:
             print(login)
             query = "UPDATE users SET trafficForDay=%s, lastUpdate=%s WHERE login=%s"
+            conn, cursor = get_connection()
             cursor.execute(query, (traffic, last_update, login))
             conn.commit()
 
 query = "UPDATE users SET trafficForDay=\"0\" WHERE CURTIME() >= \"23:30:00\""
+conn, cursor = get_connection()
 cursor.execute(query)
 conn.commit()
+
+cursor.close()
+conn.close()
+
 
