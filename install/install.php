7644ecbb8753f8d02473be7a598a5b1e7f69f51e
Sizov Sergei <sizovIT@gmail.com>
2014-12-18 17:03:55 +0500

    -- бооольше исправлений

--- install/install.php
+++ install/install.php
@@ -1,9 +1,9 @@
 <?php
 
 //сначала проверим заполнен ли файл конфигурации
-require_once('checkconf.php');
+require_once(__DIR__ . 'checkconf.php');
 
-$checkConf = new Checkconf();
+$checkConf = new Config();
 
 if( !empty($checkConf->LdapIp) &&
     !empty($checkConf->LdapLogin) &&
@@ -23,21 +23,21 @@ if( !empty($checkConf->LdapIp) &&
 )
 {
 //создаем БД и добавляем туда таблицы для будущей работы
-    shell_exec( "mysql -u ".$MysqlRootLogin." -p".$MysqlRootPassword." < ".$sldapDirectory."/install/ldap_squid.sql" );
+    shell_exec( "mysql -u ".$checkConf->MysqlRootLogin." -p".$checkConf->MysqlRootPassword." < ".$checkConf->sldapDirectory."/install/ldap_squid.sql" );
     echo "\n База данных с таблицами создана, перехожу к созданию конфигурационного файла squid.conf \n";
 
 //создаем конфигурационный файл squid.conf
-    shell_exec( "php -f ".$sldapDirectory."/install/scripts/squid_conf_writer.php " );
-    echo "\n Конфигурационный файл squid.conf создан ".$sldapDirectory."/squid.conf, вы можете скормить его squid полностью \n или скопировать из него и частями вставить в основной конфигурационный файл squid.\n Перехожу к добавлению парсера логов в cron \n";
+    shell_exec( "php -f ".$checkConf->sldapDirectory."/install/scripts/squid_conf_writer.php " );
+    echo "\n Конфигурационный файл squid.conf создан ".$checkConf->sldapDirectory."/squid.conf, вы можете скормить его squid полностью \n или скопировать из него и частями вставить в основной конфигурационный файл squid.\n Перехожу к добавлению парсера логов в cron \n";
 
 //Формируем путь к парсеру логов squid и добавляем его в cron
-    $handler = fopen( $sldapDirectory."/install/scripts/crontask", "w" );
+    $handler = fopen( $checkConf->sldapDirectory."/install/scripts/crontask", "w" );
 
-    fwrite( $handler, "*/1 * * * * cd ".$sldapDirectory." && php -f parser.php \n" );
+    fwrite( $handler, "*/1 * * * * cd ".$checkConf->sldapDirectory." && php -f parser.php \n" );
 
     fclose( $handler );
 
-    shell_exec( "/usr/bin/crontab ".$sldapDirectory."/install/scripts/crontask" );
+    shell_exec( "/usr/bin/crontab ".$checkConf->sldapDirectory."/install/scripts/crontask" );
     echo "\n Парсер логов сформирован и добавлен в cron \n";
 
 
